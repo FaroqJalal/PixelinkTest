0382fba231a0d90470ccd6aa1dc664c765aa1a27
FaroqJalal <gmovies48@gmail.com>
2024-12-25 16:30:49 +0300

    Add files via upload

---
+++ JS/x.js
@@ -0,0 +1,15 @@
+function func1(){
+    document.querySelector('.active1').style.opacity="1";
+    document.querySelector('.active2').style.opacity="0";
+    document.querySelector('.active3').style.opacity="0";
+}
+function func2(){
+    document.querySelector('.active1').style.opacity=0;
+    document.querySelector('.active2').style.opacity=1;
+    document.querySelector('.active3').style.opacity=0;
+}
+function func3(){
+    document.querySelector('.active1').style.opacity=0;
+    document.querySelector('.active2').style.opacity=0;
+    document.querySelector('.active3').style.opacity=1;
+}
