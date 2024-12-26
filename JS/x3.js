ff2910ea60afc76bcedc502c54288378aff7905b
pixelink <gmovies48@gmail.com>
2024-12-26 04:41:13 +0300

    Update x3.js

--- JS/x3.js
+++ JS/x3.js
@@ -14,6 +14,25 @@ function func3(){
     document.querySelector('.active3').style.opacity=1;
 }
 
-//
+let button1=document.querySelector('.mobile-menu-button');
+button1.addEventListener('click',(e)=>{
+    e.stopPropagation(); //important
+    document.querySelector('.mobile-div-lang').style.opacity=1;
+        const element = document.querySelector('.mobile-div-lang');
+        if (element) {
+            element.style.pointerEvents = 'all'; // Ensures pointer events are enabled
+        }
+});
 
-//
+    
+
+
+document.body.addEventListener('click', (event) => {
+    const element = document.querySelector('.mobile-div-lang');
+    
+    if (element) {
+        // Apply opacity and disable pointer events whenever a click happens
+        element.style.opacity = 0;
+        element.style.pointerEvents = 'none';
+    }
+});
